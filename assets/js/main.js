e07e0d57104d15bb2d6b82e993c0e5560ec99a6c
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-05-25 12:13:44 -0400

    Agregadas funciones y eventos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,79 @@
+// declaracion de variables y constantes
+
 const budgetInput = document.querySelector('#budget-input');
 const budgetValue = document.querySelector('#budget-total');
 const budgetExpenses = document.querySelector('#expenses-total');
 const budgetBalance = document.querySelector('#balance');
 const budgetBtn = document.querySelector('#budget-btn');
 
-const expense = document.querySelector('#expense');
+const expenseName = document.querySelector('#expense');
 const expenseInput = document.querySelector('#expense-input');
 const expenseBtn = document.querySelector('#expense-btn');
 
+const expenseList = document.querySelector('#expense-list');
+const deleteBtn = document.querySelector('#delete-btn');
+const expenseValue = document.querySelector('#expense-value');
+
 let budgetTotal = 0;
 let expensesTotal = 0;
-let balanceTotal = `$ ${budgetTotal - expensesTotal}`;
+const expensesArr = [];
 
-budgetExpenses.textContent = `$ ${expensesTotal}`;
-budgetBalance.textContent = `$ ${balanceTotal}`;
 
-    budgetBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        budgetTotal = budgetInput.value;
-        expensesTotal = parseInt(budgetExpenses.value);
-        budgetValue.textContent = `$ ${budgetTotal}`;
-        budgetBalance.textContent = balanceTotal;
-        console.log(budgetTotal);
+// funciones
+
+const renderExpenses = (expensesArr) => {
+    expensesArr.map(expense => {
+        
+        expenseList.innerHTML += `
+            <tr>
+                <td>${expense.name}</td>
+                <td>$ <span id="expense-amount">${expense.amount}</span></td>
+                <td><button id="delete-btn" class="btn btn-danger">X</button></td>
+            </tr>
+        `;
     });
+};
+
+const addExpense = () => {
+    const expense = {
+        name: expenseName.value,
+        amount: expenseInput.value
+    };
+    expensesArr.push(expense);
+    expenseList.innerHTML = '';
+    renderExpenses(expensesArr);
+    expensesTotal += parseInt(expenseInput.value);
+    budgetExpenses.textContent = `$ ${expensesTotal}`;
+    budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+};
+
+const deleteExpense = (e) => {
+    if (e.target.id === 'delete-btn') {
+        const expenseAmount = e.target.parentElement.parentElement.querySelector('#expense-amount').textContent;
+        expensesTotal -= parseInt(expenseAmount);
+        budgetExpenses.textContent = `$ ${expensesTotal}`;
+        budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+        e.target.parentElement.parentElement.remove();
+    }
+};
+
+// eventos
+
+budgetBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    budgetTotal = budgetInput.value;
+    budgetValue.textContent = `$ ${budgetTotal}`;
+    budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+});
 
 
+expenseBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addExpense();
+    }
+);
 
-
-
-
+expenseList.addEventListener('click', (e) => {
+    deleteExpense(e);
+    } 
+);
