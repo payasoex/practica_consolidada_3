446092522d1141c2dbbaaaaa39beb61a801c625b
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-05-26 12:06:24 -0400

    funcionalidad de almacenamiento en localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,28 +14,52 @@ const expenseList = document.querySelector('#expense-list');
 const deleteBtn = document.querySelector('#delete-btn');
 const expenseValue = document.querySelector('#expense-value');
 
+const clearBtn = document.querySelector('#clear-btn');
 
 // variables
 
-let budgetTotal = 0;
-let expensesTotal = 0;
-const expensesArr = [];
-
+let budgetTotal = localStorage.getItem('budget') || 0;
+let expensesTotal = localStorage.getItem('expensesTotal') || 0;
+let expensesArr = JSON.parse(localStorage.getItem('expenses')) || [];
+console.log(expensesArr);
 
 // funciones
+
+function inicialize() {
+    budgetValue.textContent = `$ ${budgetTotal}`;
+    budgetExpenses.textContent = `$ ${expensesTotal}`;
+    budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+    renderExpenses(expensesArr);
+    console.log(localStorage.getItem('expenseTotal'));
+}
 
 const renderExpenses = (expensesArr) => {
     expensesArr.map(expense => {
         
         expenseList.innerHTML += `
-            <tr>
-                <td>${expense.name}</td>
-                <td>$ <span id="expense-amount">${expense.amount}</span></td>
-                <td><button id="delete-btn" class="btn btn-danger">X</button></td>
-            </tr>
+        <tr>
+        <td>${expense.name}</td>
+        <td>$ <span id="expense-amount">${expense.amount}</span></td>
+        <td><button id="delete-btn" class="btn btn-danger">X</button></td>
+        </tr>
         `;
     });
 };
+
+// const addExpense = () => {
+//     const expense = {
+//         name: expenseName.value,
+//         amount: expenseInput.value
+//     };
+//     expensesArr.push(expense);
+//     localStorage.setItem('expenses', JSON.stringify(expensesArr));// al local storage
+//     console.log(localStorage.getItem('expenses')) 
+//     expenseList.innerHTML = '';
+//     renderExpenses(expensesArr);
+//     expensesTotal += parseInt(expenseInput.value);
+//     budgetExpenses.textContent = `$ ${expensesTotal}`;
+//     budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+// };
 
 const addExpense = () => {
     const expense = {
@@ -43,6 +67,9 @@ const addExpense = () => {
         amount: expenseInput.value
     };
     expensesArr.push(expense);
+    localStorage.setItem('expenses', JSON.stringify(expensesArr));
+    localStorage.setItem('expensesTotal', expensesTotal);
+    console.log(localStorage.getItem('expensesTotal'))
     expenseList.innerHTML = '';
     renderExpenses(expensesArr);
     expensesTotal += parseInt(expenseInput.value);
@@ -50,41 +77,78 @@ const addExpense = () => {
     budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
 };
 
+
+// const deleteExpense = (e) => {
+//     if (e.target.id === 'delete-btn') {
+//         const expenseAmount = e.target.parentElement.parentElement.querySelector('#expense-amount').textContent;
+//         expensesTotal -= parseInt(expenseAmount);
+//         budgetExpenses.textContent = `$ ${expensesTotal}`;
+//         budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+//         e.target.parentElement.parentElement.remove();
+//         localStorage.setItem('expenses', JSON.stringify(expensesArr));
+//     }
+// };
+
 const deleteExpense = (e) => {
+    const expenseAmount = e.target.parentElement.parentElement.querySelector('#expense-amount').textContent;
+    const expenseName = e.target.parentElement.parentElement.querySelector('td').textContent;
     if (e.target.id === 'delete-btn') {
-        const expenseAmount = e.target.parentElement.parentElement.querySelector('#expense-amount').textContent;
         expensesTotal -= parseInt(expenseAmount);
         budgetExpenses.textContent = `$ ${expensesTotal}`;
         budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
         e.target.parentElement.parentElement.remove();
+        expensesArr = expensesArr.filter(expense => expense.name !== expenseName);
+        localStorage.setItem('expenses', JSON.stringify(expensesArr));
+        localStorage.setItem('expensesTotal', expensesTotal)
     }
+};
+
+const clearBudgetAndExpenses = () => {
+    localStorage.removeItem('budget');
+    localStorage.removeItem('expenses');
+    localStorage.removeItem('expensesTotal');
 };
 
 // eventos
 
 budgetBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (budgetInput.value > 0) {
+    if (budgetInput.value > 0 && budgetInput.value !== '') {
         budgetTotal = budgetInput.value;
         budgetValue.textContent = `$ ${budgetTotal}`;
         budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+        localStorage.setItem('budget', budgetTotal);
+        console.log(localStorage.getItem('budget'));
     } else {
         alert('El presupuesto debe ser un número mayor a 0');
-        }
+    }
 });
 
 
 expenseBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (expenseInput.value > 0) {
+    if (expenseName.value !== '' && expenseInput.value > 0) {
         addExpense();
     } else {
-        alert('El gasto debe ser un número mayor a 0');
-        }
+        alert('Debes ingresar el nombre del gasto y el gasto debe ser un número mayor a 0');
     }
-);
+});
 
 expenseList.addEventListener('click', (e) => {
     deleteExpense(e);
-    } 
-);
+});
+
+clearBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    clearBudgetAndExpenses();
+    budgetTotal = 0;
+    expensesTotal = 0;
+    budgetValue.textContent = `$ ${budgetTotal}`;
+    budgetExpenses.textContent = `$ ${expensesTotal}`;
+    budgetBalance.textContent = `$ ${budgetTotal - expensesTotal}`;
+    expenseList.innerHTML = '';
+    expensesArr = [];
+});
+
+// inicializar
+inicialize();
